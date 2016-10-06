a87c2944d865f0a0156f985488d2dcf5db148dbf
Arthur <canalarthur@yahoo.fr>
2016-10-06 16:08:51 +0200

    POULET

---
+++ src/fr/character/enemies/Enemy.java
@@ -0,0 +1,17 @@
+package fr.character.enemies;
+
+import fr.character.Player;
+
+public class Enemy {
+
+	public void collPlayer(Player player) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public int getLife() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}

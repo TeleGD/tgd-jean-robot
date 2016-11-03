d6f56380a8f5600e679df7d5180e9bccff7e6d3a
Macbook de Stefy <Stefy@Mac-de-Marielle-2.local>
2016-11-03 16:44:39 +0100

    suite des bonus

--- src/fr/bonus/IncreaseSpeed.java
+++ src/fr/bonus/IncreaseSpeed.java
@@ -1,9 +1,12 @@
 package fr.bonus;
 
+import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.StateBasedGame;
+
+import fr.characters.Player;
 
 public class IncreaseSpeed extends Bonus{
 
@@ -11,21 +14,21 @@ public class IncreaseSpeed extends Bonus{
 	/**
 	 * augmente la vitesse sur l'axe des x
 	 */
-	public void comportment() {
-		// TODO Auto-generated method stub
-		
+	public void comportment(Player player) {
+		this.tempsActivation = System.currentTimeMillis();
+		player.setSpeedX(player.getSpeedX()*2);
+		// TODO verriefier si le wait est juste pour la fonction ou pour tout le jeu
+		while (this.tempsActivation+this.duration>System.currentTimeMillis()){
+			//la boucle permet d'attendre la durée duration
+		}
+		player.setSpeedX(player.getSpeedX()/2);
 	}
-
-	@Override
-	public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException {
-		// TODO Auto-generated method stub
-		
-	}
-
+	
 	@Override
 	public void update(GameContainer container, StateBasedGame game, int delta) throws SlickException {
 		// TODO Auto-generated method stub
 		
 	}
 
+	
 }
--- src/fr/bonus/InverseControl.java
+++ src/fr/bonus/InverseControl.java
@@ -5,15 +5,21 @@ import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.StateBasedGame;
 
+import fr.characters.Player;
+
 public class InverseControl extends Bonus{
 
 	@Override
 	/**
 	 * multiplie par -1 la vitesse celon l'axe x
 	 */
-	public void comportment() {
-		// TODO Auto-generated method stub
-		
+	public void comportment(Player player) {
+		player.setSpeedX(-player.getSpeedX());
+		while (this.tempsActivation+this.duration>System.currentTimeMillis()){
+			//la boucle permet d'attendre la durée duration
+		}
+		player.setSpeedX(-player.getSpeedX());
+		//TODO créer une varriable inverseControl dans le player + une autre fonction de récupération des inputs + un if dans la fonction update
 	}
 
 	@Override
